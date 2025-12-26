af9608986f8c8ccc548ee8a872ea7db8ba701b01
Immanuel Mwendia Maina <imaina671@gmail.com>
2025-12-26 03:24:55 +0300

    Fix: Add S3 and CloudFront hostnames to next.config.ts for image optimization

--- web/next.config.ts
+++ web/next.config.ts
@@ -4,6 +4,22 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
   output: 'standalone', // Required for Docker
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'faithconnectapp.s3.af-south-1.amazonaws.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'd25pwvs6zlto08.cloudfront.net',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
